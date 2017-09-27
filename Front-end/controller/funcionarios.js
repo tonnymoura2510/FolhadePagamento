39c407429f43103b212247b10869be229957f08c
Antoniely Ribeiro <tonnymoura@hotmail.com>
2017-09-26 21:24:38 -0300

    alteração cargo
    cargo e cargo.js

--- Front-end/controller/funcionarios.js
+++ Front-end/controller/funcionarios.js
@@ -4,7 +4,7 @@ funcionariosModulo.controller("funcionariosController", function ($scope){
 	$scope.funcionarios = [
 	{codigo: 1, nome: 'Edson', cpf: '990.952.381-72', cargo:'Analista'},
 	{codigo: 2, nome: 'João', cpf: '200.952.381-11' , cargo:'Programador'},
-	{codigo: 3, nome: 'Paulo', cpf: '001.952.381-98', cargo:'Operador'},
+	{codigo: 3, nome: 'Paulo', cpf: '001.952.381-98', cargo:'Gerente'},
 	{codigo: 4, nome: 'Sérgio', cpf: '101.952.381-98', cargo:'Programador'},
 	];
 

2ecd417a01731ff7aa8765090ef8a3c57051993f
inacioedson <inacioedson@hotmail.com>
2017-09-28 22:46:05 -0300

    Implementando cargos no controller pra passar os dados pro select
    Implementando cargos no controller pra passar os dados pro select

--- Front-end/controller/funcionarios.js
+++ Front-end/controller/funcionarios.js
@@ -1,11 +1,19 @@
 var funcionariosModulo = angular.module('funcionariosModulo',[]);
 
 funcionariosModulo.controller("funcionariosController", function ($scope){
+	
+	$scope.cargos = [
+	{codigo: 1, nome: 'Vendedor', descricao:'Vender Produtos', salario: 'R$1.500,00'}, 
+	{codigo: 2, nome: 'Coordenador', descricao: 'Coordena a Produção', salario: 'R$1.700,00'},
+	{codigo: 3, nome: 'Operador', descricao: 'Opera as máquinas', salario: 'R$1.300,00'},
+	{codigo: 4, nome: 'Atendente', descricao: 'Atendimento ao cliente', salario: 'R$1.100,00'},
+	];
+	
 	$scope.funcionarios = [
-	{codigo: 1, nome: 'Edson', cpf: '990.952.381-72', cargo:'Analista'},
-	{codigo: 2, nome: 'João', cpf: '200.952.381-11' , cargo:'Programador'},
-	{codigo: 3, nome: 'Paulo', cpf: '001.952.381-98', cargo:'Gerente'},
-	{codigo: 4, nome: 'Sérgio', cpf: '101.952.381-98', cargo:'Programador'},
+	{codigo: 1, nome: 'Edson', cpf: '990.952.381-72', cargo:{codigo: 1, nome: 'Vendedor', descricao:'Vender Produtos', salario: 'R$1.500,00'}},
+	{codigo: 2, nome: 'João', cpf: '200.952.381-11' , cargo:{codigo: 2, nome: 'Coordenador', descricao: 'Coordena a Produção', salario: 'R$1.700,00'}},
+	{codigo: 3, nome: 'Paulo', cpf: '001.952.381-98', cargo:{codigo: 3, nome: 'Operador', descricao: 'Opera as máquinas', salario: 'R$1.300,00'}},
+	{codigo: 4, nome: 'Sérgio', cpf: '101.952.381-98', cargo:{codigo: 4, nome: 'Atendente', descricao: 'Atendimento ao cliente', salario: 'R$1.100,00'}},
 	];
 
 	$scope.selecionaFuncionario = function(funcionarioSelecionado){
